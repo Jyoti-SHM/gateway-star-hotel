9f2c603318617e5b6dc2fcd6ced7c9fddc8c8f2c
Jyoti-SHM <jyotihospitalityminds@gmail.com>
2025-05-15 00:39:09 +0530

    Add files via upload

---
+++ contactform/contactthanks.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	
+	<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="icon" type="image/png" href="imgs/favicon/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="imgs/favicon/favicon.svg" />
+<link rel="shortcut icon" href="imgs/favicon/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="imgs/favicon/apple-touch-icon.png" />
+</head>
+
+<body class="contact-body">
+
+	<div id="page-wrap">
+
+		<br /><br />
+		
+			
+		<h1>Your message has been sent!</h1><br />
+		
+		<p><a href="../contact.html">Back to Contact Form</a></p>
+	
+	</div>
+	
+
+
+</body>
+
+</html>
